d95e22b5b4a13692e6279709027f2f80e72d4bb9
Pratik Sharma <118264213+pratiksh21@users.noreply.github.com>
2024-09-11 10:47:13 +0530

    Create 00_Imporatant_Points.js

---
+++ 00_Imporatant_Points.js
@@ -0,0 +1,7 @@
+-------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>>> Next.js
+
+1. Unlike React, which lacks native routing, Next.js provides seamless routing functionality out of the box. Additionally, it supports server-side rendering, improving performance and SEO.
+2.Faster Development: Next.js accelerates development by offering built-in features and conventions. Developers can focus on building features rather than configuring complex setups.
+3.SEO Optimization: Next.js enhances SEO by addressing slow rendering and loading times associated with client-side rendering. Its SSR capabilities ensure that search engines can efficiently crawl and index your content.
+
+  ----Next.js is a powerful and flexible React framework that has quickly become popular among developers for building server-side rendered and static web applications. Created by Vercel, Next.js simplifies the process of developing modern web applications with its robust feature set.
